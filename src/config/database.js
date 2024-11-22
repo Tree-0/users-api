9bd78c1c598ea36bfb15597d83d7107a308d1980
Tree-0 <117487029+Tree-0@users.noreply.github.com>
2024-11-21 23:20:02 -0600

    config and endpoints

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,13 @@
-const { Pool } = require("pg");
-require("dotenv").config();
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is required");
 }
 
-const pool = new Pool({
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
@@ -14,3 +16,5 @@ const pool = new Pool({
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
 });
+
+
